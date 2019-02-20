46656a331d0508baa589a9a08ada7517e963d528
bella <564414779@qq.com>
2019-02-20 19:27:12 +0000

    hello
    hello

---
+++ week2.py
@@ -0,0 +1,57 @@
+#name = name.upper()
+#print("Hello {}!".format(name))
+#print("hello bella!")
+#print("What's your name?")
+#name = input()
+#print("Hello {}!".format(name))
+def greet(name):
+    print("Hello,"+name+". Good morning!")
+#greet("bella")
+def add_two_numbers():
+    number1 = 1
+    number2 = 2
+    answer = number1 + number2
+    print("{} plus {} is {}".format(number1, number2, answer))
+
+#add_two_numbers()
+
+def add_two_numbers_and_return_value():
+    number1 = 1
+    number2 = 2
+    answer = number1 + number2
+    return answer
+
+returned_value = add_two_numbers_and_return_value()
+#print(returned_value)
+empty_list = []
+numbers = [0, 1, 2, 3, 4, 5]
+fruits = ["pineapples", "oranges", "bananas"]
+mixed = [15, "sunshine", "jumper", 4, "sky"]
+#print(numbers[4])
+#print(fruits[0])
+#print(mixed[2])
+my_shopping_cart = ["cake", "plates", "plastic forks", "juice", "cups"]
+
+#for item in my_shopping_cart:
+  #print(item)
+
+#for x in my_shopping_cart:
+  #print("hello!")
+  #print(x)
+number = input("Enter a number between 1 and 10: ")
+
+#print(number)
+
+number = int(number)
+
+#if number > 10:
+	#print("Too high!")
+
+#if number <= 0: 
+   #print("Too low!")
+def equal(value):
+	if value==1：
+	   print("ok")
+	else：
+	   print("not ok")
+equal(2)
